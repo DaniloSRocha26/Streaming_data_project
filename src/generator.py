6ac85be8992d3fdd6513f21003f2499143fcc45b
Danilo.Dev <Danilo.Santana26@Outlook.com.br>
2026-03-25 19:12:42 -0300

    feat: add churn logic and cancel dates for user generator

--- src/generator.py
+++ src/generator.py
@@ -1,4 +1,5 @@
 from faker import Faker
+from datetime import timedelta
 import random
 fake = Faker()
 
@@ -10,7 +11,7 @@ plans = {
     "PREMIUM": "24.99"
 }
 
-GENRES = [
+genres = [
     "Action",
     "Comedy",
     "Drama",
@@ -23,28 +24,37 @@ GENRES = [
     "Animation"
 ]
 
-    
+def churnUsers(subscription_date):
+    is_Active_User = random.random()
+    if is_Active_User <= 0.8:
+        days_after = timedelta(days=random.randint(1, 30))
+        return f"canceled on {subscription_date + days_after}"
+    else:
+        return "subscribed"
 
 
-    
+
 def randomUserGenerator():
       for _ in range(1000):
         user_plan = random.choice(list(plans.keys()))
+        subscription_date = fake.date_between(start_date='-1m')
         user = {
             "name": fake.name(),
             "email": fake.email(),
             "age": random.randint(18,80),
             "country": fake.country(),
-            "plan": random.choice(plans),
+            "plan": user_plan,
             "monthly_fee": plans[user_plan],
-            "subscription_date": fake.date_between(start_date = '-1m'),
+            "subscription_date": str(subscription_date),
             "watch_hours_per_month": random.randint(10, 70),
-            "is_active": 
-            "cancel_date"
-            "favorite_genre"   
-            
-          }  
+            "is_active": churnUsers(subscription_date),
+            "favorite_genre": random.choice(genres)
+          }
+        usersList.append(user)
     
+randomUserGenerator()
 
+for user in usersList:
+    print(user)
 
 
