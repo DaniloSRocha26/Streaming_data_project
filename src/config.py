e0998c209e686c45a70e7862adf0dfb09e556a4a
Danilo.Dev <182436555+DaniloSRocha26@users.noreply.github.com>
2026-03-26 10:20:18 -0300

    feat: add database connection config

--- src/config.py
+++ src/config.py
@@ -2,7 +2,8 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-# Connects to MongoDB using MONGO_URI and inserts a test user
+# Connects to MongoDB using MONGO_URI
+load_dotenv()
 
 mongo_uri = os.getenv("MONGO_URI")
 client = MongoClient(mongo_uri)
@@ -10,7 +11,3 @@ client = MongoClient(mongo_uri)
 
 database = client["Streaming_Data"]
 collection = database["users"]
-
-collection.insert_one({"name": "Danilo", "age": 23})
-
-print("user inserted")
